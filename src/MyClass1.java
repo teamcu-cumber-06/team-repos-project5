e7fc5a67059d93089c5253bad920023b46664f46
prishupatel <prishupatel218@gmail.com>
2021-11-03 12:38:21 -0400

    line 4 and 5 sout.

--- src/MyClass1.java
+++ src/MyClass1.java
@@ -1,5 +1,7 @@
 public class MyClass1 {
     public static void main(String[] args) {
         System.out.println("First task of project 5");
+
+        System.out.println("This is update by Kishan");
     }
 }
